6528d476a077a97f6d246c263de132890c2fe839
yosr aguir <aguiryosr@gmail.com>
2020-11-19 21:31:05 +0100

    register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+
+  get username(){
+    return this.loginForm.get('username')
+  }
+  get password(){
+    return this.loginForm.get('password')
+  }
+
+  public errorMessages = {
+    username : [
+      {type : 'required' , message: 'user name is required'},
+      {type : 'pattern' , message: 'Please entera valid email address'}
+    ],
+    password : [
+      {type : 'required' , message: 'password is required'},
+      {type : 'minLength' , message: 'Password can be longer than 8 characters'}
+    ]
+  }
+
+  constructor(public FormBuilder: FormBuilder) { //formbuiller intermédaire entre le formgroup et le forme control name
+    this.loginForm = FormBuilder.group({
+      username : [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+.[a-zA-z0-z0-9]+$")
+          
+        ])
+      ],
+      password: [
+        "",
+        Validators.compose([
+          Validators.required, 
+          Validators.minLength(8)
+        ])
+      ],
+    });
+  }
 
   ngOnInit() {
   }
+   public submit() {
+     console.log(this.loginForm.value);
+   }
 
 }
